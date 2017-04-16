69794af35b5a994ec58d8b49ea68a62088f727cd
gunilla <gunilla1148@gmail.com>
2017-04-16 17:30:19 +0800

    implement step 5

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,10 +3,16 @@ class JobsController < ApplicationController
 
   def show
     @job = Job.find(params[:id])
+
+    if @job.is_hidden
+      flash[:warning] = "This Job already archived"
+      redirect_to root_path
+    end 
+
   end
 
   def index
-    @jobs = Job.where(:is_hidden => false)
+    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
   end
 
   def new
